75c52364f38c232d10f6dd81224f600619cc84f8
yagyesh-senslyze <yagyesh.bobde@senslyze.com>
2025-04-16 02:06:54 +0530

    feat: add clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Waitlist } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+      
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -24,7 +26,7 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-
+        <Waitlist />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
